a584548fa6b80a3c92ba045fa3918825c577ec56
upvitaly <upvitaly13@gmail.com>
2021-05-11 21:52:16 +0600

    add passport package

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable 
 {
