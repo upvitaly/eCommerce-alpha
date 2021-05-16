f3b0442d24fe9b9bb7fab2062730de0a983c4e74
upvitaly <upvitaly13@gmail.com>
2021-05-16 14:25:35 +0600

    add view order page

---
+++ resources/views/admin/order/view_order.blade.php
@@ -0,0 +1,142 @@
+@extends('admin.admin_master')
+
+@section('admin')
+
+    <div class="sl-pagebody">
+        <div class="sl-page-title">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Order</strong> Details
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>{{ $order->name }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Phone</th>
+                                        <th>{{ $order->phone }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Payment Type</th>
+                                        <th>{{ $order->payment_type }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Payment Id</th>
+                                        <th>{{ $order->payment_id }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Total</th>
+                                        <th>${{ $order->total }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Month</th>
+                                        <th>{{ $order->month }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>{{ $order->date }}</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Shipping</strong> Details
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>{{ $shipping->ship_name }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Phone</th>
+                                        <th>{{ $shipping->ship_phone }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Email</th>
+                                        <th>{{ $shipping->ship_email }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Address</th>
+                                        <th>{{ $shipping->ship_address }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>City</th>
+                                        <th>${{ $shipping->ship_city }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Status:</th>
+                                        <th>
+                                            @if ($order->status == 0)
+                                                <span class="badge badge-warning">pending</span>
+                                            @elseif($order->status==1)
+                                                <span class="badge badge-info">Payment Accept</span>
+                                            @elseif($order->status==2)
+                                                <span class="badge badge-info">Progress</span>
+                                            @elseif($order->status==3)
+                                                <span class="badge badge-success">Delivered</span>
+                                            @else
+                                                <span class="badge badge-danger">Cancle</span>
+                                            @endif
+                                        </th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div><!-- sl-page-title -->
+        <div class="row">
+            <div class="card pd-20 pd-sm-40">
+                <h6 class="card-body-title">Product Details</h6>
+
+
+                <div class="table-wrapper">
+                    <table id="datatable1" class="table display responsive nowrap">
+                        <thead>
+                            <tr>
+                                <th class="wd-15p">Product Id</th>
+                                <th class="wd-15p">Product Name</th>
+                                <th class="wd-15p">Image</th>
+                                <th class="wd-15p">Color</th>
+                                <th class="wd-15p">Size</th>
+                                <th class="wd-15p">Quantity</th>
+                                <th class="wd-15p">Single Price</th>
+                                <th class="wd-15p">Total Price</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($details as $row)
+                                <tr>
+                                    <td>{{ $row->product_id }}</td>
+                                    <td>{{ $row->product_name }}</td>
+                                    <td><img src="{{ URL::to($row->image_one) }}" width="115px" height="115px"></td>
+                                    <td>{{ $row->color }}</td>
+                                    <td>{{ $row->size }}</td>
+                                    <td>{{ $row->quantity }}</td>
+                                    <td>{{ $row->singleprice }}</td>
+                                    <td>{{ $row->totalprice }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div><!-- table-wrapper -->
+            </div><!-- card -->
+        </div>
+
+    </div><!-- sl-mainpanel -->
+@endsection
